254ed8061972d3095cba03616641e06dd6ee467f
DM <dommcneill@outlook.com>
2022-10-24 18:42:19 +0100

    trying to rotate player smoothly

--- Ball_Game_Unity/Assets/Scripts/Player/PlayerScript.cs
+++ Ball_Game_Unity/Assets/Scripts/Player/PlayerScript.cs
@@ -1,183 +1,215 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerScript : MonoBehaviour
 {
-	[SerializeField]
-	private float minimumDistance = .2f;
-	[SerializeField]
-	private float maximumTime = 1f;
-	[SerializeField, Range(0f, 1f)]
-	private float directionThreshold = .9f;
-	[SerializeField]
-	private float rotationAmmount;
+    [SerializeField]
+    private float minimumDistance = .2f;
+    [SerializeField]
+    private float maximumTime = 1f;
+    [SerializeField, Range(0f, 1f)]
+    private float directionThreshold = .9f;
+    [SerializeField]
+    private float rotationAmmount;
 
-	public Transform vertiualCamera;
-	public Transform orientation;
-	public Transform mainCamera;
-
-
-	private InputManager inputManager;
-
-	private Rigidbody rb;
-
-	private Vector2 startPosision;
-	private float startTime;
-	private Vector2 endPosision;
-	private float endTime;
-
-	private float yRotation;
-
-	private Coroutine coroutine;
-
-	void Awake()
-	{
-		inputManager = InputManager.Instance;
-		rb = GetComponent<Rigidbody>();
-	}
-
-	void OnEnable()
-	{
-		inputManager.OnStartPrimaryTouch += SwipeStart;
-		inputManager.OnEndPrimaryTouch += SwipeEnd;
-	}
-
-	void OnDisable()
-	{
-		inputManager.OnStartPrimaryTouch -= SwipeStart;
-		inputManager.OnEndPrimaryTouch -= SwipeEnd;
-	}
-
-	private void SwipeStart(Vector2 pos, float time)
-	{
-		startPosision = pos;
-		startTime = time;
-	}
-
-	private void SwipeEnd(Vector2 pos, float time)
-	{
-
-		endPosision = pos;
-		endTime = time;
-		DetectSwipe();
-	}
+    public Transform VertuialCamera;
+    public Transform Orientation;
+    public Transform MainCamera;
+    public GameObject PlayerModel;
 
 
+    private InputManager inputManager;
 
-	private void DetectSwipe()
-	{
-		if (Vector3.Distance(startPosision, endPosision) >= minimumDistance && (endTime - startTime) <= maximumTime)
-		{
-			Vector3 direction = endPosision - startPosision;
-			Vector2 direction2D = new Vector2(direction.x, direction.y).normalized;
-			SwipeDirection(direction2D);
-		}
-	}
+    private Rigidbody rb;
 
-	private void SwipeDirection(Vector2 direction)
-	{
-		//print(direction);
-		float percentOfSwipe = Vector2.Dot(Vector2.up, direction);
-		float rotationY = 180 * vertiualCamera.rotation.y; // try local with main camera
+    private Vector2 startPosision;
+    private float startTime;
+    private Vector2 endPosision;
+    private float endTime;
 
-		if (Vector2.Dot(Vector2.up, direction) > directionThreshold)
+    private float yRotation;
+
+    private Coroutine coroutine;
+
+    private float endY;
+    private float startY;
+
+    private float time;
+
+
+    void Awake()
+    {
+        inputManager = InputManager.Instance;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update() // UPDATE
+    {
+        if (PlayerModel.transform.rotation.y == Orientation.rotation.y)
+            time = 0;
+        else
+            time += Time.deltaTime * 1f;
+
+        PlayerModel.transform.rotation = Quaternion.Euler(PlayerModel.transform.rotation.x, Mathf.Lerp(PlayerModel.transform.rotation.y, Orientation.rotation.y, time), PlayerModel.transform.rotation.z);
+    }
+    
+    private void rotatePlayer(float start, float end, float speed = 1)
+    {
+        
+    }
+
+    void OnEnable()
+    {
+        inputManager.OnStartPrimaryTouch += SwipeStart;
+        inputManager.OnEndPrimaryTouch += SwipeEnd;
+    }
+
+    void OnDisable()
+    {
+        inputManager.OnStartPrimaryTouch -= SwipeStart;
+        inputManager.OnEndPrimaryTouch -= SwipeEnd;
+    }
+
+    private void SwipeStart(Vector2 pos, float time)
+    {
+        startPosision = pos;
+        startTime = time;
+    }
+
+    private void SwipeEnd(Vector2 pos, float time)
+    {
+        endPosision = pos;
+        endTime = time;
+        DetectSwipe();
+    }
+
+
+    private void DetectSwipe()
+    {
+        if (Vector3.Distance(startPosision, endPosision) >= minimumDistance && (endTime - startTime) <= maximumTime)
         {
-			orientation.transform.rotation = Quaternion.Euler(orientation.rotation.x, rotationY, orientation.rotation.z);
-			rb.AddForce(orientation.forward, ForceMode.Impulse);
-		}
-		else if (direction.x > 0) // right
-		{
-			
-			print("right " + percentOfSwipe);
+            Vector3 direction = endPosision - startPosision;
+            Vector2 direction2D = new Vector2(direction.x, direction.y).normalized;
+            SwipeDirection(direction2D);
+        }
+    }
 
-			if (direction.y > 0) // can use the whole 90 to -90 / 1 to -1
-			{
-				// top right 90 to 0
-				percentOfSwipe = 1 - percentOfSwipe; // inverts the percent so is 0 to 90
+    private void SwipeDirection(Vector2 direction)
+    {
+        //print(direction);
+        float percentOfSwipe = Vector2.Dot(Vector2.up, direction);
+        float rotationY = 180 * VertuialCamera.rotation.y; // try local with main camera
 
-				float ninetyPercent = 90 * percentOfSwipe;
+        if (Vector2.Dot(Vector2.up, direction) > directionThreshold)
+        {
+            Orientation.transform.rotation = Quaternion.Euler(Orientation.rotation.x, rotationY, Orientation.rotation.z);
+            rb.AddForce(Orientation.forward, ForceMode.Impulse);
+        }
+        else if (direction.x > 0) // right
+        {
 
-				float ammountToRotate = rotationY + ninetyPercent;
+            print("right " + percentOfSwipe);
 
-				orientation.transform.rotation = Quaternion.Euler(orientation.rotation.x, ammountToRotate, orientation.rotation.z);
-				rb.AddForce(orientation.forward, ForceMode.Impulse);
+            if (direction.y > 0) // can use the whole 90 to -90 / 1 to -1
+            {
+                // top right 90 to 0
+                percentOfSwipe = 1 - percentOfSwipe; // inverts the percent so is 0 to 90
 
-			}
-			else
-			{
-				// bottom right 90 to 180
+                float ninetyPercent = 90 * percentOfSwipe;
 
-				float ninetyPercent = 90 * -percentOfSwipe; // look at this -Var might not work
+                float ammountToRotate = rotationY + ninetyPercent;
 
-				float ammountToRotate = (rotationY + 90) + ninetyPercent;
+                Orientation.transform.rotation = Quaternion.Euler(Orientation.rotation.x, ammountToRotate, Orientation.rotation.z);
+                rb.AddForce(Orientation.forward, ForceMode.Impulse);
 
-				orientation.transform.rotation = Quaternion.Euler(orientation.rotation.x, ammountToRotate, orientation.rotation.z);
-				rb.AddForce(orientation.forward, ForceMode.Impulse);
+            }
+            else
+            {
+                // bottom right 90 to 180
 
-			}
-			
-		}
-		else // left
-		{
-			print("Left " + percentOfSwipe);
+                float ninetyPercent = 90 * -percentOfSwipe; // look at this -Var might not work
 
-			if (direction.y > 0)
-			{
-				// top left, -90 to 0
+                float ammountToRotate = (rotationY + 90) + ninetyPercent;
 
-				percentOfSwipe = 1 - percentOfSwipe; // inverts the percent so is 0 to -90
+                Orientation.transform.rotation = Quaternion.Euler(Orientation.rotation.x, ammountToRotate, Orientation.rotation.z);
+                rb.AddForce(Orientation.forward, ForceMode.Impulse);
 
-				float ninetyPercent = 90 * -percentOfSwipe; // look at this -Var might not work
+            }
 
-				float ammountToRotate = rotationY + ninetyPercent;
+        }
+        else // left
+        {
+            print("Left " + percentOfSwipe);
 
-				orientation.transform.rotation = Quaternion.Euler(orientation.rotation.x, ammountToRotate, orientation.rotation.z);
-				rb.AddForce(orientation.forward, ForceMode.Impulse);
+            if (direction.y > 0)
+            {
+                // top left, -90 to 0
 
+                percentOfSwipe = 1 - percentOfSwipe; // inverts the percent so is 0 to -90
 
-			}
-			else
-			{
+                float ninetyPercent = 90 * -percentOfSwipe; // look at this -Var might not work
 
-				// bottom left -90 to -180
-				float ninetyPercent = 90 * percentOfSwipe;
+                float ammountToRotate = rotationY + ninetyPercent;
 
-				float ammountToRotate = (rotationY - 90) + ninetyPercent;
-
-				orientation.transform.rotation = Quaternion.Euler(orientation.rotation.x, ammountToRotate, orientation.rotation.z);
-				rb.AddForce(orientation.forward, ForceMode.Impulse);
-				
-			}
-		}
+                Orientation.transform.rotation = Quaternion.Euler(Orientation.rotation.x, ammountToRotate, Orientation.rotation.z);
+                rb.AddForce(Orientation.forward, ForceMode.Impulse);
 
 
-		//print(Vector2.Dot(Vector2.up, direction));
-		
-		//if (Vector2.Dot(Vector2.up, direction) > directionThreshold)
-		//{
-		//	//print("swiped up");
-		//	rb.AddForce(orientation.forward, ForceMode.Impulse);
-		//}
-		//else if (Vector2.Dot(Vector2.right, direction) > directionThreshold)
-		//{
-		//	//print("swiped right");
-		//	yRotation += rotationAmmount;
-		//	//transform.rotation = Quaternion.Euler(transform.rotation.x + rotationAmmount, transform.rotation.y, transform.rotation.z);
-		//	orientation.transform.rotation = Quaternion.Euler(orientation.rotation.x, yRotation, orientation.rotation.z);
+            }
+            else
+            {
 
-		//}
-		//else if (Vector2.Dot(Vector2.down, direction) > directionThreshold)
-		//{
-		//	//print("swiped down");
-		//	rb.AddForce(-orientation.forward, ForceMode.Impulse);
-		//}
-		//else if (Vector2.Dot(Vector2.left, direction) > directionThreshold)
-		//{
-		//	//print("swiped left");
-		//	yRotation -= rotationAmmount;
-		//	//transform.rotation = Quaternion.Euler(transform.rotation.x - rotationAmmount, transform.rotation.y, transform.rotation.z);
-		//	orientation.transform.rotation = Quaternion.Euler(orientation.rotation.x, yRotation, orientation.rotation.z);
-		//}
-	}
+                // bottom left -90 to -180
+                float ninetyPercent = 90 * percentOfSwipe;
+
+                float ammountToRotate = (rotationY - 90) + ninetyPercent;
+
+                Orientation.transform.rotation = Quaternion.Euler(Orientation.rotation.x, ammountToRotate, Orientation.rotation.z);
+                rb.AddForce(Orientation.forward, ForceMode.Impulse);
+
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        //print(Vector2.Dot(Vector2.up, direction));
+
+        //if (Vector2.Dot(Vector2.up, direction) > directionThreshold)
+        //{
+        //	//print("swiped up");
+        //	rb.AddForce(Orientation.forward, ForceMode.Impulse);
+        //}
+        //else if (Vector2.Dot(Vector2.right, direction) > directionThreshold)
+        //{
+        //	//print("swiped right");
+        //	yRotation += rotationAmmount;
+        //	//transform.rotation = Quaternion.Euler(transform.rotation.x + rotationAmmount, transform.rotation.y, transform.rotation.z);
+        //	Orientation.transform.rotation = Quaternion.Euler(Orientation.rotation.x, yRotation, Orientation.rotation.z);
+
+        //}
+        //else if (Vector2.Dot(Vector2.down, direction) > directionThreshold)
+        //{
+        //	//print("swiped down");
+        //	rb.AddForce(-Orientation.forward, ForceMode.Impulse);
+        //}
+        //else if (Vector2.Dot(Vector2.left, direction) > directionThreshold)
+        //{
+        //	//print("swiped left");
+        //	yRotation -= rotationAmmount;
+        //	//transform.rotation = Quaternion.Euler(transform.rotation.x - rotationAmmount, transform.rotation.y, transform.rotation.z);
+        //	Orientation.transform.rotation = Quaternion.Euler(Orientation.rotation.x, yRotation, Orientation.rotation.z);
+        //}
+    }
 }
